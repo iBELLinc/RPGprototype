01f65ae3b3701ca092d81656d87a767f0a23f5b2
Ian <ibell.michael@gmail.com>
2022-01-25 14:05:51 -0700

    Created Item hierarchy for Item/Tool/Weapon/Melee/Sword; Initial scripts created for interactPickup() created.

--- RPG Prototype-1/Assets/Scripts/Characters/Player/PlayerInteract.cs
+++ RPG Prototype-1/Assets/Scripts/Characters/Player/PlayerInteract.cs
@@ -4,15 +4,36 @@ using UnityEngine;
 
 public class Interact : MonoBehaviour
 {
+    private Rigidbody rb;
+    private string INTERACT = "Interact";
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = this.GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        // If player presses E
+        if (Input.GetButtonDown(INTERACT))
+        {
+            // Are they near an item that they can pick up? interactPickup();
+            if (null) {}
+            // Are they near a door or container? => interactOpen();
+            // Are they near an NPC? interactSpeak();
+        }
     }
+
+    // Player picks up item
+    private void interactPickup()
+    {
+        // Send call to PickupController script for handling
+    }
+
+    // Player opens door/container
+    private void interactOpen() {}
+    // Player enters dialogue with NPC
+    private void interactSpeak() {}
 }
